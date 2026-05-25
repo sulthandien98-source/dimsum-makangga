7552da583ca8973d4ab1fd1430f2cbe414550185
Sulthan <sulthandien98@gmail.com>
2026-05-25 18:34:21 +0700

    fix railway proxy session

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,10 +15,27 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware) {
 
-        // TRUST PROXY RAILWAY
-        $middleware->trustProxies(at: '*');
+        /*
+        |--------------------------------------------------------------------------
+        | TRUST PROXIES (RAILWAY HTTPS)
+        |--------------------------------------------------------------------------
+        */
 
-        // ALIAS MIDDLEWARE
+        $middleware->trustProxies(
+            at: '*',
+            headers:
+                Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
+
+        /*
+        |--------------------------------------------------------------------------
+        | MIDDLEWARE ALIAS
+        |--------------------------------------------------------------------------
+        */
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
