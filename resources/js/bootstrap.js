f1e6be59e0dd822c1a30a31201d983e4202b96f3
Sulthan <sulthandien98@gmail.com>
2026-05-25 13:11:39 +0700

    Fix Tailwind production build

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,16 @@
-import axios from 'axios';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-window.axios = axios;
+window.Pusher = Pusher;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+    wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
+    wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 443),
+
+    forceTLS: false,
+    enabledTransports: ['ws'],
+});
