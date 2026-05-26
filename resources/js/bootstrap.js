9e757d99f085d899940b2fa509b4a275e315817c
Sulthan <sulthandien98@gmail.com>
2026-05-26 20:21:46 +0700

    Disable Reverb temporarily

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,22 +3,23 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+// Reverb / Pusher dinonaktifkan sementara
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
 
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+//     wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
 
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
 
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
 
-    forceTLS: true,
+//     forceTLS: true,
 
-    enabledTransports: ['ws', 'wss'],
-});
+//     enabledTransports: ['ws', 'wss'],
+// });
