0a2de13bcf6c1a5bd3cea64f29a56bf8998eee26
Sulthan <sulthandien98@gmail.com>
2026-05-30 02:55:20 +0700

    Final UI Fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,27 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const themeToggle = document.getElementById('theme-toggle');
+
+if (
+    localStorage.getItem('theme') === 'dark' ||
+    (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
+
+if (themeToggle) {
+    themeToggle.addEventListener('click', () => {
+        document.documentElement.classList.toggle('dark');
+
+        if (document.documentElement.classList.contains('dark')) {
+            localStorage.setItem('theme', 'dark');
+        } else {
+            localStorage.setItem('theme', 'light');
+        }
+    });
+}
